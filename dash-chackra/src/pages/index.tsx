14b4012b699098a3533743a36ba6e7fb4b56d623
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-05-19 19:18:19 -0300

    Update index.tsx

--- dash-chackra/src/pages/index.tsx
+++ dash-chackra/src/pages/index.tsx
@@ -1,7 +1,35 @@
+import { Flex, Button, Stack } from "@chakra-ui/react";
+
+import { Input } from "../components/form/input";
+
 export default function Home() {
   return (
     <div>
-      <h1>inicio</h1>
+      <Flex
+        width={"100vw"}
+        height={"100vh"}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
+        <Flex
+          as="form"
+          width={"100%"}
+          maxWidth={360}
+          bg="gray.800"
+          p="8"
+          borderRadius={8}
+          flexDir={"column"}
+        >
+          <Stack spacing={5}>
+            <Input name="Email" label="Email" type="Email" />
+            <Input name="Password" label="Password" type="Password" />
+          </Stack>
+
+          <Button type="submit" mt="6" colorScheme={"pink"}>
+            Login
+          </Button>
+        </Flex>
+      </Flex>
     </div>
   );
 }
