c95f9a9b4e20e9833e270242ba68a5d7e0b448cd
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-05-30 13:06:23 -0300

    tela de criação do usuario

---
+++ dash-chackra/src/pages/users/create.tsx
@@ -0,0 +1,53 @@
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  HStack,
+  SimpleGrid,
+  VStack,
+} from "@chakra-ui/react";
+import { Header } from "../../components/header";
+import { SideBar } from "../../components/SideBar";
+import { Input } from "../../components/form/input";
+
+export default function CreateUser() {
+  return (
+    <Box>
+      <Header />
+      <Flex width="100%" my={6} maxWidth={1480} mx="auto" px={6}>
+        <SideBar />
+
+        <Box flex={1} borderRadius={8} bg="gray.800" p={8}>
+          <Heading size="lg" fontWeight="normal">
+            Create User
+          </Heading>
+          <Divider my={6} borderColor="gray.700" />
+          <VStack spacing={8}>
+            <SimpleGrid minChildWidth={240} spacing={8} w="100%">
+              <Input name="name" label="Full Name" />
+              <Input name="email" label="Email" />
+            </SimpleGrid>
+
+            <SimpleGrid minChildWidth={240} spacing={8} w="100%">
+              <Input name="Password" type="password" label="Passowrd" />
+              <Input
+                name="Password_confirmation"
+                type="password"
+                label="Confirmation Passowrd"
+              />
+            </SimpleGrid>
+          </VStack>
+
+          <Flex mt={8} justifyContent="flex-end">
+            <HStack spacing={4}>
+              <Button colorScheme="whiteAlpha">Cancel</Button>
+              <Button colorScheme="pink">Confirm</Button>
+            </HStack>
+          </Flex>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
